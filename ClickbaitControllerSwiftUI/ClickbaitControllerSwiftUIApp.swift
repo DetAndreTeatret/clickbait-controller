f3325f0f0d9ef34ae4bd16dc61e6dc07fb510106
KingSimon <19822231+KingOfSquares@users.noreply.github.com>
2022-09-28 18:57:36 +0200

    Initial Commit

---
+++ ClickbaitControllerSwiftUI/ClickbaitControllerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickbaitControllerSwiftUIApp.swift
+//  ClickbaitControllerSwiftUI
+//
+//  Created by Simon on 28/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClickbaitControllerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickbaitControllerSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClickbaitControllerSwiftUI
+//
+//  Created by Simon on 28/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
